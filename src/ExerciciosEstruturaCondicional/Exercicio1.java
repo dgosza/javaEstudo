fd9c25307dd784e86fd3cd4defb142518eb30df1
diego <diegomagasouza@gmail.com>
2019-02-03 21:16:30 -0200

    estudo

---
+++ src/ExerciciosEstruturaCondicional/Exercicio1.java
@@ -0,0 +1,21 @@
+package ExerciciosEstruturaCondicional;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+
+
+        Scanner leia = new Scanner(System.in);
+        System.out.println("enter w a integer number");
+        int n1 = leia.nextInt();
+
+        if(n1<0){
+            System.out.println("Negative");
+        }else{
+            System.out.println("Positive");
+        }
+
+    }
+}
---
+++ src/ExerciciosEstruturaCondicional/Exercicio7.java
@@ -0,0 +1,29 @@
+package ExerciciosEstruturaCondicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio7 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.ENGLISH);
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Digite o x");
+        double x = leia.nextDouble();
+        System.out.println("Digite o y");
+        double y = leia.nextDouble();
+
+        if(x==0.00 & y == 0.00){
+            System.out.println("Origem");
+        }else if(x > 0 && y > 0){
+            System.out.println("Q1");
+        }else if(x > 0 && y <0){
+            System.out.println("Q4");
+        }else if(x<0 && y>0){
+            System.out.println("Q2");
+        }else if(x<0 && y<0){
+            System.out.println("Q3");
+        }
+
+    }
+}
