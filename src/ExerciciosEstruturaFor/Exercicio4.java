76f9699ad780275cb8f789c1deb0bc081e47a10c
diego <diegomagasouza@gmail.com>
2019-02-09 19:45:46 -0200

    estudo

---
+++ src/ExerciciosEstruturaFor/Exercicio4.java
@@ -0,0 +1,16 @@
+package ExerciciosEstruturaFor;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.ENGLISH);
+        Scanner leia = new Scanner(System.in);
+
+
+
+        leia.close();
+    }
+}
