1436ee16bf0ec02f9691d27f7a89d183795e8aa3
diego <diegomagasouza@gmail.com>
2019-03-10 19:34:43 -0300

    estudo

--- src/OrientacaoObjeto/Modulo2/methods/Product.java
+++ src/OrientacaoObjeto/Modulo2/methods/Product.java
@@ -2,19 +2,23 @@ package OrientacaoObjeto.Modulo2.methods;
 
 public class Product {
 
-    public String name;
-    public double price;
-    public int quantity;
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product(){
+
+    }
 
     public Product(String name, double price, int quantity){
         this.name = name;
-        this.price = price;
+        this.price= price;
         this.quantity= quantity;
     }
 
     public Product(String name, double price){
         this.name = name;
-        this.price = price;
+        this.price= price;
     }
 
 
@@ -38,6 +42,6 @@ public class Product {
                 + quantity
                 + " units, Total: $ "
                 + String.format("%.2f", totalValueInStock());
-
     }
+
 }
