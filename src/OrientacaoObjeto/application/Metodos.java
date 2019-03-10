5dcd6859581e746e0a4346ae3401e46a2850063a
diego <diegomagasouza@gmail.com>
2019-03-09 23:02:45 -0300

    estudo

---
+++ src/OrientacaoObjeto/application/Metodos.java
@@ -0,0 +1,29 @@
+package OrientacaoObjeto.application;
+
+public class Metodos {
+
+    public static void calculaProcedimento(int a, int b){
+
+        int s  = a + b;
+        System.out.println("A soma é "+s);
+
+    }
+
+    public static int calculaFuncao(int a, int b){
+        int s = a + b;
+        return s;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.print("Chamada do Procedimento");
+        calculaProcedimento(5,2);
+
+        int recebeReturn = calculaFuncao(10,2);
+        System.out.print("Chamada da Função"+ recebeReturn);
+
+    }
+
+
+}
