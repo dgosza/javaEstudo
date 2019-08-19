ac940fdcb4d783fc3c2fe47121abf429adf3997e
Diego Souza <diegomagasouza@gmail.com>
2019-08-19 14:48:10 -0300

    wrapper

---
+++ src/aulasDevDojo/wrapper/Main.java
@@ -0,0 +1,25 @@
+package aulasDevDojo.wrapper;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Byte byteWrapper = 1;
+        Short shortWrapper = 1;
+        Integer integerWrapper = 1;
+        Float floatWrapper = 3f;
+        Double doubleWrapper = 13d;
+        Long longWrapper = 34l;
+        Boolean booWrapper = Boolean.TRUE;
+
+        String stringWrapper = null;
+        char charWrapper = 'a' ;
+
+        String value = "313";
+
+        Float f = Float.parseFloat(value);
+        System.out.println(Character.isDigit('a'));
+
+
+
+    }
+}
