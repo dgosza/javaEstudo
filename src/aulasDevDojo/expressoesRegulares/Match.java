035d59f20b0e7f288bbd186e59f1e83efbc3e9f1
Diego Souza <diegomagasouza@gmail.com>
2019-08-27 15:28:27 -0300

    estudo

--- src/aulasDevDojo/expressoesRegulares/Match.java
+++ src/aulasDevDojo/expressoesRegulares/Match.java
@@ -1,8 +1,5 @@
 package aulasDevDojo.expressoesRegulares;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Match {
     public static void main(String[] args) {
 
@@ -25,12 +22,39 @@ public class Match {
         System.out.println("HÁ uma ocorrencia de String? " + verifica6);
 
         boolean verifica7 = "A3".matches("..");
-        System.out.println("HÁ DUAS OCORRENCIAS DE STRING ?"+verifica7);
+        System.out.println("HÁ DUAS OCORRENCIAS DE STRING ?" + verifica7);
 
         String cep = "\\d\\d\\d\\d\\d-\\d\\d\\d";
         boolean verifica8 = "99855-999".matches(cep);
-        System.out.println("CEP VALIDO ?"+verifica8);
+        System.out.println("CEP VALIDO ?" + verifica8);
+
+        boolean verifica9 = "23".matches("\\d{2}");
+        System.out.println("Há 2 numeros ?" + verifica9);
+
+        boolean verifica10 = "23".matches("\\d{9,}");
+        System.out.println(verifica10);
 
 
+
+        boolean palavra = "Java322".matches("^Java.*");
+        System.out.println(palavra);
+
+        //Termina com 322
+        //O ponto (.) Começa com qualquer caractere e busca 0
+        //ou mais caracteres finalizando com o num 322
+        palavra = "Java322".matches(".*322$");
+        System.out.println(palavra);
+
+        //Pesquisa se uma palavra existe no texto
+        palavra = "Hello World Java".matches(".*Java.*");
+        System.out.println(palavra);
+
+        //Pesquisa os caracteres que estão depois da letra O e antes da palavra Java
+        palavra = "O mundo Java".matches("^O.*Jaava$");
+        System.out.println(palavra);
+
+        //Pesquisa pela palavra Inter ou Grêmio
+        boolean time = "Grêmio".matches("Inter|Grêmio");
+        System.out.println("Time: "+time);
     }
 }
